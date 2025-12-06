d518c6379908f2a4686b42aa17b7070dd959b575
Mathilo <debramutheu05@gmail.com>
2025-12-06 16:39:04 +0300

    Ternary Operator

---
+++ Ternaryexample.java
@@ -0,0 +1,12 @@
+public class Ternaryexample
+{
+    public static void main(String [] args)
+    {
+        int a=10;
+        int b=20;
+        int larger=(a>b)?a:b;
+        System.out.println(larger);
+    }
+}
+//Syntax
+//condition ? value_if_true : value_if_false
